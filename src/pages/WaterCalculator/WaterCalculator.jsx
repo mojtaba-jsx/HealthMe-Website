7c751472602a34ba6f5b4393410d1da1e7fd3ede
mojtaba-jsx <mojtaba.jsx@gmail.com>
2024-11-10 08:06:32 +0330

    [WaterCalculator]- Add Some Changes

--- src/pages/WaterCalculator/WaterCalculator.jsx
+++ src/pages/WaterCalculator/WaterCalculator.jsx
@@ -12,7 +12,7 @@ function WaterCalculator() {
           <div className="water-calculator__info">
             <h1 className="water-calculator__info-title">
               محاسبه کننده آب مورد نیاز بدن
-              <GiWaterBottle/>
+              <GiWaterBottle />
             </h1>
             <h2 className="water-calculator__info-subtitle">
               چه میزان آب و مایعات در طول روز بنوشیم؟
@@ -25,62 +25,64 @@ function WaterCalculator() {
               بزنیم.هر سلول و ارگان در بدن ما برای اینکه بتواند به درستی کار کند
               نیاز به آب دارد، آب مایعی حیاتی در بدن ما می باشد .
             </p>
-            <img
-              src="./images/water-info.png"
-              alt="water"
-              className="water-calculator__info-image"
-            />
-            <form className="water-calculator__form">
-              <label className="water-calculator__form-label">
-                وزن (کیلوگرم):
-                <input
-                  type="number"
-                  value={80}
-                  className="water-calculator__form-input"
-                />
-              </label>
+            <div className="water-calculator__wrapper">
+              <img
+                src="./images/water-info.png"
+                alt="water"
+                className="water-calculator__info-image"
+              />
+              <form className="water-calculator__form">
+                <label className="water-calculator__form-label">
+                  وزن (کیلوگرم):
+                  <input
+                    type="number"
+                    value={80}
+                    className="water-calculator__form-input"
+                  />
+                </label>
 
-              <label className="water-calculator__form-label">
-                سن:
-                <input
-                  type="number"
-                  value={30}
-                  className="water-calculator__form-input"
-                />
-              </label>
+                <label className="water-calculator__form-label">
+                  سن:
+                  <input
+                    type="number"
+                    value={30}
+                    className="water-calculator__form-input"
+                  />
+                </label>
 
-              <label className="water-calculator__form-label">
-                جنسیت:
-                <select className="water-calculator__form-select">
-                  <option value="male">مرد</option>
-                  <option value="female">زن</option>
-                </select>
-              </label>
+                <label className="water-calculator__form-label">
+                  جنسیت:
+                  <select className="water-calculator__form-select">
+                    <option value="male">مرد</option>
+                    <option value="female">زن</option>
+                  </select>
+                </label>
 
-              <label className="water-calculator__form-label">
-                سطح فعالیت:
-                <select className="water-calculator__form-select">
-                  <option value="low">کم</option>
-                  <option value="medium">متوسط</option>
-                  <option value="high">زیاد</option>
-                </select>
-              </label>
+                <label className="water-calculator__form-label">
+                  سطح فعالیت:
+                  <select className="water-calculator__form-select">
+                    <option value="low">کم</option>
+                    <option value="medium">متوسط</option>
+                    <option value="high">زیاد</option>
+                  </select>
+                </label>
 
-              <label className="water-calculator__form-label">
-                شرایط آب و هوایی:
-                <select className="water-calculator__form-select">
-                  <option value="hot">گرم</option>
-                  <option value="temperate">معتدل</option>
-                  <option value="cold">سرد</option>
-                </select>
-              </label>
+                <label className="water-calculator__form-label">
+                  شرایط آب و هوایی:
+                  <select className="water-calculator__form-select">
+                    <option value="hot">گرم</option>
+                    <option value="temperate">معتدل</option>
+                    <option value="cold">سرد</option>
+                  </select>
+                </label>
 
-              <button className="water-calculator__btn">محاسبه</button>
-              <span className="water-calculator__result">
-                میزان آب مورد نیاز شما برابر است با :
-                <span className="water-calculator__result-value">40</span>
-              </span>
-            </form>
+                <button className="water-calculator__btn">محاسبه</button>
+                <span className="water-calculator__result">
+                  میزان آب مورد نیاز شما برابر است با :
+                  <span className="water-calculator__result-value">40</span>
+                </span>
+              </form>
+            </div>
           </div>
         </div>
         <Footer />
